de7ff82ef83cfc0f7c5134e46defefbb59080cf5
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-04-11 11:41:17 -0300

    Commit novo estudo

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,23 @@
+
+public class TestaConversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14f;
+		
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		long numeroGrande = 651611513646131511l;
+		short valorPequeno = 2133;
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+
+}
