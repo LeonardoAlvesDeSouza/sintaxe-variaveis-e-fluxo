fe86edf5366da525c6568e3aeb2d6e1e1b94a355
Leonardo Alves <104588087+Zhorg10@users.noreply.github.com>
2023-04-13 17:24:01 -0300

    aprendizado novo sobre int e boolean

--- sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -2,19 +2,16 @@ public class TestaCondicional2 {
 
 	public static void main(String[] args) {
 		System.out.println("testando condicionais");
-		int idade = 16;
-		int quantidadePessoas = 3;
+		int idade = 20;
+		int quantidadePessoas = 1;
+		boolean acompanhado = quantidadePessoas >= 2;
 
-		if (idade >= 18) {
+		System.out.println("valor de acompanhado = " + acompanhado);
+		if (idade >= 18 && acompanhado) {
 			System.out.println("você tem mais de 18 anos");
 			System.out.println("seja bem vindo");
-		} else {
-			if (quantidadePessoas >= 2) {
-				System.out.println("você não tem 18, mas pode entrar, " + "pois está acompanhado");
-			} else {
+		}else {
 				System.out.println("infelizmente você não pode entrar");
-			}
 		}
-				
 	}
 }
